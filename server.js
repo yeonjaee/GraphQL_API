1c535001bd65a140a3ace67c1afba386aa31cfa3
steve <siriyay@naver.com>
2023-01-08 22:57:39 +0900

    query

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-import { ApolloServer,gql} from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 
 // explain the shape of the data to GraphQL
 // using GraphQL SDK
 const typeDefs = gql`
-    type Query {
-        text: String
-        allFilms: String
-        film: String
-}
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User!
+  }
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet!
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
 `;
 
-const server = new ApolloServer({typeDefs})
+const server = new ApolloServer({ typeDefs });
 
-
-server.listen().then(({url})=>{
+server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
-})
+});
 
