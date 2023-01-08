8e684fd993f975ca0106908aa8cf2071eef5c73c
steve <siriyay@naver.com>
2023-01-08 22:55:44 +0900

    Revert "1"
    This reverts commit 98eaf61250df89ff4d4b4113a556d3163b71c0fc.

--- server.js
+++ server.js
@@ -3,18 +3,10 @@ import { ApolloServer,gql} from "apollo-server";
 // explain the shape of the data to GraphQL
 // using GraphQL SDK
 const typeDefs = gql`
-    type User{
-    id:ID
-    username: String
-    }
-    type Tweet {
-    id:ID
-    text:String
-    author: User
-    }
     type Query {
-       allTweets: [Tweet]
-tweet(id: ID):  Tweet
+        text: String
+        allFilms: String
+        film: String
 }
 `;
 
