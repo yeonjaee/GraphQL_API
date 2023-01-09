29240202082895153fb3922cceb1820b804fb478
steve <siriyay@naver.com>
2023-01-10 00:22:44 +0900

    resolver

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets=[
+    {
+        id : "1",
+        text: "first one",
+    },
+    {
+        id : "2",
+        text: "second one",
+    }
+]
+
 // explain the shape of the data to GraphQL
 // using GraphQL SDK
 const typeDefs = gql`
@@ -12,11 +23,12 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    ping: String!
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
@@ -28,7 +40,18 @@ const typeDefs = gql`
 // POST DELETE PUT /api/v1/tweets
 // GET /api/v1/tweet/:id
 
-const server = new ApolloServer({ typeDefs });
+// resolver 는 하나의 로직. 하나의 객체.
+const resolvers = {
+    Query:{
+        allTweets(){
+            return tweets;
+        },
+        tweet(root, {id}){
+            return tweets.find(tweet => tweet.id === id);
+        }
+    }
+}
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
