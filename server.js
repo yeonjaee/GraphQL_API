4f01274d530a950970b8593328c003f861bc6f24
steve <siriyay@naver.com>
2023-01-11 23:12:22 +0900

    mutation resolvers

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const tweets=[
+let tweets = [
     {
-        id : "1",
-        text: "first one",
+        id: "1",
+        text: "first one!",
     },
     {
-        id : "2",
+        id: "2",
         text: "second one",
-    }
-]
+    },
+];
 
-// explain the shape of the data to GraphQL
-// using GraphQL SDK
 const typeDefs = gql`
   type User {
-    id: ID
+    id: ID!
     username: String!
     firstName: String!
     lastName: String
@@ -28,32 +26,45 @@ const typeDefs = gql`
   type Query {
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
-    ping: String!
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
 `;
-
 // GET /api/v1/tweets
 // POST DELETE PUT /api/v1/tweets
 // GET /api/v1/tweet/:id
 
-// resolver 는 하나의 로직. 하나의 객체.
 const resolvers = {
-    Query:{
-        allTweets(){
+    Query: {
+        allTweets() {
             return tweets;
         },
-        tweet(root, {id}){
-            return tweets.find(tweet => tweet.id === id);
-        }
-    }
-}
+        tweet(root, { id }) {
+            return tweets.find((tweet) => tweet.id === id);
+        },
+    },
+    Mutation: {
+        postTweet(_, { text, userId }) {
+            const newTweet = {
+                id: tweets.length + 1,
+                text,
+            };
+            tweets.push(newTweet);
+            return newTweet;
+        },
+        deleteTweet(_, { id }) {
+            const tweet = tweets.find((tweet) => tweet.id === id);
+            if (!tweet) return false;
+            tweets = tweets.filter((tweet) => tweet.id !== id);
+            return true;
+        },
+    },
+};
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
 });
-
