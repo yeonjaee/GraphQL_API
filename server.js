3fdf4058026c2ecf846cbeb86d2285eb5fca64c4
steve <siriyay@naver.com>
2023-01-12 23:18:22 +0900

    relatinoships

--- server.js
+++ server.js
@@ -4,19 +4,34 @@ let tweets = [
     {
         id: "1",
         text: "first one!",
+        userId: "2",
     },
     {
         id: "2",
         text: "second one",
+        userId: "1",
+    },
+];
+
+let users = [
+    {
+        id: "1",
+        firstName: "how",
+        lastName: "gee",
+    },
+    {
+        id: "2",
+        firstName: "Gang",
+        lastName: "gang",
     },
 ];
 
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
-    lastName: String
+    lastName: String!
+    fullName: String!
   }
   type Tweet {
     id: ID!
@@ -24,6 +39,7 @@ const typeDefs = gql`
     author: User
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
@@ -44,12 +60,17 @@ const resolvers = {
         tweet(root, { id }) {
             return tweets.find((tweet) => tweet.id === id);
         },
+        allUsers() {
+            console.log("allUsers called!");
+            return users;
+        },
     },
     Mutation: {
         postTweet(_, { text, userId }) {
             const newTweet = {
                 id: tweets.length + 1,
                 text,
+                userId,
             };
             tweets.push(newTweet);
             return newTweet;
@@ -59,6 +80,16 @@ const resolvers = {
             if (!tweet) return false;
             tweets = tweets.filter((tweet) => tweet.id !== id);
             return true;
+        },
+    },
+    User: {
+        fullName({ firstName, lastName }) {
+            return `${firstName} ${lastName}`;
+        },
+    },
+    Tweet: {
+        author({ userId }) {
+            return users.find((user) => user.id === userId);
         },
     },
 };
